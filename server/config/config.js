fe4a06986bc97b2b3f82d56eb659e852f41ae262
LitionGeek <alanalexis97@hotmail.com>
2021-11-01 21:13:02 -0300

    Api rest usuarios

---
+++ server/config/config.js
@@ -0,0 +1,32 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Vencimiento de Token
+// ============================
+process.env.CADUCIDAD_TOKEN = 60*60*24*30;
+
+// ============================
+//  SEED de auth
+// ============================
+process.env.SEED = process.env.SEED || 'seed-desarrollo';
+
+// ============================
+//  Base de datos
+// ============================
+
+let urlDB;
+if(process.env.NODE_ENV ==='dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = process.env.MONGO_URI;
+} 
+
+process.env.URLDB = urlDB;
